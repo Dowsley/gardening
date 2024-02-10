9624f5fc30fd7c51d153bdfa91dded9059a890b9
J. F. Dowsley <joao.dowsley850@gmail.com>
2024-02-10 13:37:14 -0300

    Add sky background

--- src/Main.cs
+++ src/Main.cs
@@ -1,10 +1,36 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 public partial class Main : Node2D
 {
-	public override void _Ready()
-	{
-		
-	}
+    private List<Node2D> _availableSkyBackgrounds = new List<Node2D>();
+
+    private int _currSkyBackground = 0;
+    
+    public override void _Ready()
+    {
+        foreach (var child in GetNode("BG").GetChildren())
+        {
+            _availableSkyBackgrounds.Add((Node2D)child);
+        }
+    }
+
+    public override void _Process(float delta)
+    {
+        if (Input.IsActionJustPressed("ui_accept"))
+        {
+            SetNextSkyBackground();
+        }
+    }
+
+    private void SetNextSkyBackground()
+    {
+        _availableSkyBackgrounds[_currSkyBackground].Visible = false;
+        _currSkyBackground += 1;
+        if (_currSkyBackground >= _availableSkyBackgrounds.Count)
+        {
+            _currSkyBackground = 0;
+        }
+        _availableSkyBackgrounds[_currSkyBackground].Visible = true;
+    }
 }
